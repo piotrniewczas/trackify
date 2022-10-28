445c2cb04630662b8470a0dd9f0f56af4d718c3c
Piotr Niewczas <piotr.niewczas@movecloser.pl>
2022-10-28 09:04:22 +0200

    total  - shipping

--- src/events/sales/purchase.event.ts
+++ src/events/sales/purchase.event.ts
@@ -1,24 +1,24 @@
-import {AnalyticsEvent} from "../../interfaces/analytics-event";
-import {AbstractSalesEvent} from "./abstract-sales.event";
-import {PurchaseConfig} from "../../interfaces/events/config";
+import { AnalyticsEvent } from "../../interfaces/analytics-event";
+import { AbstractSalesEvent } from "./abstract-sales.event";
+import { PurchaseConfig } from "../../interfaces/events/config";
 
 export class PurchaseEvent extends AbstractSalesEvent implements AnalyticsEvent<PurchaseConfig> {
   name = 'purchase';
 
-  constructor(protected config: PurchaseConfig) {
+  constructor (protected config: PurchaseConfig) {
     super(config);
   }
 
-  getData(): PurchaseConfig {
+  getData (): PurchaseConfig {
     return {
       currency: this.config.currency,
       transactionId: this.config.transactionId,
-      value: this.config.value,
+      value: (this.config.value - (this.config.shipping ?? 0)),
       affiliation: this.config.affiliation,
       coupon: this.config.coupon,
       shipping: this.config.shipping,
       tax: this.config.tax,
-      items: Array.isArray(this.config.items) ? this.config.items.map(item => ({...item})) : [],
+      items: Array.isArray(this.config.items) ? this.config.items.map(item => ({ ...item })) : [],
       discount: this.config.discount,
       customer: this.config.customer ? {
         id: this.config.customer.id
