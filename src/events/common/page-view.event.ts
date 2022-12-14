e56357f3bf1ea12a631d33827e96fabe01fd03a4
Piotr Niewczas <piotr.niewczas@movecloser.pl>
2022-12-14 17:01:48 +0100

    1.7.5 fix PageViewConfig

--- src/events/common/page-view.event.ts
+++ src/events/common/page-view.event.ts
@@ -1,21 +1,15 @@
-import {AbstractEvent} from "../abstract.event";
-import {AnalyticsEvent} from "../../interfaces/analytics-event";
-import {PageViewConfig} from "../../interfaces/events/config";
+import { AbstractEvent } from "../abstract.event";
+import { AnalyticsEvent } from "../../interfaces/analytics-event";
+import { PageViewConfig } from "../../interfaces/events/config";
 
 export class PageViewEvent extends AbstractEvent implements AnalyticsEvent<PageViewConfig> {
   public name = 'page_view';
 
-  constructor(protected config: PageViewConfig) {
+  constructor (protected config: PageViewConfig) {
     super(config);
   }
 
-  getData(): PageViewConfig {
-    return {
-      pagePath: this.config.pagePath,
-      pageTitle: this.config.pageTitle,
-      language: this.config.language,
-      currency: this.config.currency,
-      turnOffPageViewForSPA: this.config.turnOffPageViewForSPA
-    }
+  getData (): PageViewConfig {
+    return this.config
   }
 }
